a062b5d3ff1cac6988243c549095ed6342ddc00b
Trevor Chan <trevchan@bu.edu>
2023-03-31 14:18:04 -0400

    Added in data structures and a few helper functions

--- tls.c
+++ tls.c
@@ -1,34 +1,86 @@
 #include "tls.h"
+#include <signal.h>
+#define MAX_NUM_THREADS 128
 
-/*
- * This is a good place to define any data structures you will use in this file.
- * For example:
- *  - struct TLS: may indicate information about a thread's local storage
- *    (which thread, how much storage, where is the storage in memory)
- *  - struct page: May indicate a shareable unit of memory (we specified in
- *    homework prompt that you don't need to offer fine-grain cloning and CoW,
- *    and that page granularity is sufficient). Relevant information for sharing
- *    could be: where is the shared page's data, and how many threads are sharing it
- *  - Some kind of data structure to help find a TLS, searching by thread ID.
- *    E.g., a list of thread IDs and their related TLS structs, or a hash table.
- */
 
-/*
- * Now that data structures are defined, here's a good place to declare any
- * global variables.
- */
+// [[[*** Section 1: Defining data structures ***]]]
 
-/*
- * With global data declared, this is a good point to start defining your
- * static helper functions.
- */
+// TLS: Maps tid to local storage and locations
+typedef struct thread_local_storage {
+	pthread_t tid;
+	unsigned int size;	// Size in bytes
+	unsigned int page_num;	// Number of pages
+	struct page ** pages;	// Array of pointers to pages
+} TLS;
 
-/*
- * Lastly, here is a good place to add your externally-callable functions.
- */ 
+// Page: States address of page and number of threads using that specific page
+struct page {
+	unsigned int address;	// Start address of page
+	int ref_count; 	// Counter for number of shared pages
+};
+
+// Mappings between tid and TLS (used to find TLS)
+struct tid_tls_pair {
+	pthread_t tid;
+	TLS *tls;
+};
+
+
+// [[[*** Section 2: Defining global variables ***]]]
+
+// Global datastructure of tid_tls_pairs (with arbitrary MAX_NUM_THREADS)
+static TLS *tls_array[MAX_NUM_THREADS];
+
+
+
+// [[[*** Section 3: Defining helper functions ***]]]
+
+void tls_handle_page_fault(int sig, siginfo_t *si, void *context){
+	// p_fault = ((unsigned int) si->si_addr) & ~(page_size - 1);
+}
+
+// Helper function that checks if the current thread has an existing tls_array entry
+int lsa_exists() {
+	int tid = (int) pthread_self();
+	if (tls_array[tid] == NULL)
+		return 0;
+	else
+		return 1;
+}
+
+// Function to be called on first call of tls_create to initialize datastructures and signal handlers
+void tls_init() {
+
+	// Initialize signal handler
+	struct sigaction sigact;
+	// page_size = getpagesize();
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_sigaction = tls_handle_page_fault;
+	sigaction(SIGBUS, &sigact, NULL);
+	sigaction(SIGSEGV, &sigact, NULL);
+
+	// Initialize global variables
+	for (int i = 0; i < MAX_NUM_THREADS; i++){
+		tls_array[i] = NULL;
+	}
+
+}
+
+
+// [[[*** Section 4: Defining library functions ***]]]
 
 int tls_create(unsigned int size)
 {
+	// On first call initialize the data structures and signal handler
+	static int is_first_call = 1;
+	if (is_first_call) {
+		is_first_call = 0;
+		tls_init();
+	}
+
+	
+
 	return 0;
 }
 
